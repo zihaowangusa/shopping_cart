abbb0ddaa0f0609ab9dd133247b97ca4f51adc07
zihaowangusa <50635536+zihaowangusa@users.noreply.github.com>
2019-07-17 01:57:15 -0700

    Updates

---
+++ precache-manifest.faa57248fb8a0787602ee89492e4e8ab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aa35db096a6a49559e7cbd6d64b25ed",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/media/item2.2aa35db0.jpg"
+  },
+  {
+    "revision": "b61927e3d835e1b51e84",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/css/main.e5cb1465.chunk.css"
+  },
+  {
+    "revision": "30e578a2596d39c34d71",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/js/1.30e578a2.chunk.js"
+  },
+  {
+    "revision": "be01622458656c2ed36e",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/js/runtime~main.be016224.js"
+  },
+  {
+    "revision": "f8f4726fc6269ef97a7e1f0ac77a23cc",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/media/item1.f8f4726f.jpg"
+  },
+  {
+    "revision": "b61927e3d835e1b51e84",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/js/main.b61927e3.chunk.js"
+  },
+  {
+    "revision": "fe242b8145c7ea99bf07245677ee4e96",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/media/item3.fe242b81.jpg"
+  },
+  {
+    "revision": "667e69f96c7bce3a9b9913f175de68b4",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/media/item4.667e69f9.jpg"
+  },
+  {
+    "revision": "56e98852804cbebe0ac55f2481f38961",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/media/item5.56e98852.jpg"
+  },
+  {
+    "revision": "1571d9e05c160caeb3bcf88ea860533f",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/media/item6.1571d9e0.jpg"
+  },
+  {
+    "revision": "185f2b4c2080f72922d6b4a23a6ec626",
+    "url": "/zihaowangusa.github.io/shopping_cart/index.html"
+  }
+];
