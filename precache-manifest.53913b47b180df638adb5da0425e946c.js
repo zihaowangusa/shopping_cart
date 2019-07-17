3ef8aef3b158e0eabe5cf2aaf5b6a020035f0797
zihaowangusa <50635536+zihaowangusa@users.noreply.github.com>
2019-07-16 18:59:58 -0700

    Updates

---
+++ precache-manifest.53913b47b180df638adb5da0425e946c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "be01622458656c2ed36e",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/js/runtime~main.be016224.js"
+  },
+  {
+    "revision": "89c00434e67e704c8d1c",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/js/main.89c00434.chunk.js"
+  },
+  {
+    "revision": "30e578a2596d39c34d71",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/js/1.30e578a2.chunk.js"
+  },
+  {
+    "revision": "89c00434e67e704c8d1c",
+    "url": "/zihaowangusa.github.io/shopping_cart/static/css/main.e5cb1465.chunk.css"
+  },
+  {
+    "revision": "831c33ecb72247864fe89fd434910d29",
+    "url": "/zihaowangusa.github.io/shopping_cart/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.599a608c2dc6805ffeafe1f1271cc48b.js"
+  "/zihaowangusa.github.io/shopping_cart/precache-manifest.53913b47b180df638adb5da0425e946c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/zihaowangusa.github.io/shopping_cart/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
