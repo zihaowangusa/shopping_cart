55c9c62a18ed3e322c1af0e49ac1adb5430010e8
zihaowangusa <50635536+zihaowangusa@users.noreply.github.com>
2019-07-16 18:57:25 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/zihaowangusa.github.io/shopping_cart/precache-manifest.53913b47b180df638adb5da0425e946c.js"
+  "./precache-manifest.599a608c2dc6805ffeafe1f1271cc48b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/zihaowangusa.github.io/shopping_cart/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
